5c67ead8be75a1a90272e30975d03a334dcdf20a
AlmnoA <113357290+AlmnoA@users.noreply.github.com>
2023-04-20 10:11:13 -0400

    d

--- src/FullArticle.js
+++ src/FullArticle.js
@@ -1,7 +1,18 @@
 import Title from "./Components/Nav";
 import React from "react";
 
+export default function Articles(props) {
 
-export default function Articles(){
-return(<Title/>)
+    return (
+    <>
+      <Title />
+      <div className="Article-Itself">
+        <h1>{props.title}</h1>
+        <img src={props.img1}/>
+        <p>{props.text1}</p>
+        <img src={props.img2}/>
+        <p>{props.text2}</p>
+      </div>
+    </>
+  );
 }
